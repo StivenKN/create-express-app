7dce910caf94fa6568219d56ee13fbb46a845da0
ConanGH-S <blandon0207s@gmail.com>
2024-01-09 15:58:19 -0500

    fix: now app generator uses readline node package instead promptly to check visual bug

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import path from 'path'
 import colors from 'colors'
-import prompt from 'promptly'
 import createFolder from './lib/createFolder'
 import createAppStructure from './lib/createAppStructure'
 import { existsSync } from 'fs'
+import { createInterface } from 'readline'
+
+/**
+ * Interface required by node and cmd to ask project name every time is needed and start to build the expected project
+ */
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 /**
  * Async function to ask project name every time is needed and start to build the expected project
  * @returns {string}
- * @async
  * @example
  * askProjectName()
   .then(() => {doSomething...})
@@ -16,23 +23,27 @@ import { existsSync } from 'fs'
  * @author ConanGH-S
  */
 export const askProjectName = async (): Promise<string> => {
-  let projectName: string
-  do {
-    projectName = await prompt.prompt(`${colors.blue('?')} Application name: ${colors.gray('->')} `, { default: 'express-app', trim: true })
-    if (existsSync(projectName)) console.warn(colors.yellow('! Warning: ') + 'The project name already exists, please choose another one' + '\n')
-    else break
-  } while (existsSync(projectName))
-  return projectName
+  return await new Promise((resolve) => {
+    // let projectName: string = 'express-app'
+    rl.question(`${colors.blue('?')} Application name: ${colors.gray('->')} `, (response: string) => {
+      do {
+        if (existsSync(response)) console.warn(colors.yellow('! Warning: ') + 'The project name already exists, please choose another one' + '\n')
+        else {
+          resolve(response)
+          rl.close()
+          break
+        }
+      } while (existsSync(response))
+    })
+  })
 }
 
 askProjectName()
   .then((userLocation: string) => {
-    let location = userLocation.trim().replace(/ /g, '-')
+    const location = userLocation.trim().replace(/ /g, '-')
     const searchSpaces = userLocation.includes(' ')
     const currentDirectory = process.cwd()
     try {
-      if (location.length === 0) location = 'express-app'
-
       if (searchSpaces) console.warn(colors.yellow('! Warning: ') + 'Your spaces will be trimmed and converted to "-"')
       const actualPath = path.join(currentDirectory, location)
       if (actualPath !== currentDirectory) createFolder(location)
@@ -40,7 +51,6 @@ askProjectName()
         console.info(colors.green('Run: ') + colors.yellow(`cd ${location}`))
         console.info(colors.green('Run: ') + colors.yellow('npm install') + colors.green(' to install dependencies'))
         console.info(colors.green('Run: ') + colors.yellow('npm start or npm start:dev') + colors.green(' to initializate the app'))
-        process.exit(0)
       })
         .catch((err: string) => {
           console.log(colors.red(err))
@@ -51,5 +61,4 @@ askProjectName()
   })
   .catch((error) => {
     console.error(error)
-    process.exit(0)
   })
